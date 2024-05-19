e1f298067634dbacb356d4657fc4f4ee11cf5975
ParsannaK <prasanna.koirala302@gmail.com>
2024-05-19 15:13:18 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Prasanna Koirala on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
